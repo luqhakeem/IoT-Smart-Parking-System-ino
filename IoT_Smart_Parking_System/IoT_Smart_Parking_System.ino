98abb981f516105c82972bc2e920eac8d89931ed
Luqman Hakeem <75708244+luqhakeem@users.noreply.github.com>
2022-01-30 23:36:16 +0800

    Update for final version
    Remove all the unwanted library and function to finalize the final version

--- IoT_Smart_Parking_System/IoT_Smart_Parking_System.ino
+++ IoT_Smart_Parking_System/IoT_Smart_Parking_System.ino
@@ -15,8 +15,6 @@
 #include <WiFiEsp.h>
 #include <WiFiEspClient.h>
 #include <ArduinoHttpClient.h>
-//#include <MySQL_Connection.h>
-//#include <MySQL_Cursor.h>
 
 // Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
@@ -28,22 +26,17 @@ SoftwareSerial Serial1(6, 7); // RX, TX
 // ---------------------------------- //
 // WiFi Settings
 // ---------------------------------- //
-char ssid[] = "PKK_Hotspot";
-char pass[] = "ilovepkk";
+char ssid[] = "PKK_Hotspot"; // WiFi SSID
+char pass[] = "ilovepkk"; // WiFi Password
+
 int status = WL_IDLE_STATUS;
 
 // use a ring buffer to increase speed and reduce memory allocation
 RingBuffer buf(8);
 
-
-// ---------------------------------- //
-// MySQL Settings
-// ---------------------------------- //
-char user[] = "root";
-char password[] = "luq123";
   
 // ---------------------------------- //
-// Declare variables, data type and pin number
+// Declare pin number
 // ---------------------------------- //
 int US_Entrance_Trig = 2;
 int US_Entrance_Echo = 3;
@@ -67,7 +60,7 @@ Servo Servo_Exit;
 
 
 // ---------------------------------- //
-// Define variables
+// Define variables and data type
 // ---------------------------------- //
 
 long duration_1;
@@ -80,16 +73,11 @@ int ScanLot2;
 int Availability1;
 int Availability2;
 
-int pingResult;
 
 // ---------------------------------- //
 // Constant value
 // ---------------------------------- //
-const boolean debug_mode = true;
-const int limit_distance = 7; // Default value = 3cm
-const IPAddress remote_ip(192, 168, 1, 17);
-char* serverAddress = "192.168.1.17"; //Change the IP based on CMD, ipconfig
-int port = 8080;
+const int limit_distance = 7; // Default value = 7cm
 String apiKeyValue = "tPmAT5Ab3j7F9";
 char urlPayload[] = "192.168.1.17";
 
@@ -98,7 +86,6 @@ char urlPayload[] = "192.168.1.17";
 // Set pin mode (INPUT/OUTPUT)
 // ---------------------------------- //
 void setup() {
-
   pinMode(US_Entrance_Trig, OUTPUT);
   pinMode(US_Entrance_Echo, INPUT);
   pinMode(US_Exit_Trig, OUTPUT);
@@ -110,68 +97,24 @@ void setup() {
   pinMode(IR1, INPUT);
   pinMode(IR2, INPUT);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-
   Servo_Entrance.attach(ServoEntrancePin);
   Servo_Entrance.write(90);
 
   Servo_Exit.attach(ServoExitPin);
   Servo_Exit.write(90);
 
-  Serial.begin(115200);   // initialize Serial Monitor for debugging
-  Serial1.begin(115200);    // initialize Serial1 for ESP module
-  WiFi.init(&Serial1);    // initialize ESP module
+  Serial.begin(115200);   // Initialize Serial Monitor for debugging
+  Serial1.begin(115200);    // Initialize Serial1 for ESP module
+  WiFi.init(&Serial1);    // Initialize ESP module
 
-  // attempt to connect to WiFi network
+  // Attempt to connect to WiFi network
   while (status != WL_CONNECTED) {
     //Serial.print("Connecting to WiFi network: ");
     //Serial.println(ssid);
     // Connect to WPA/WPA2 network
     status = WiFi.begin(ssid, pass);
   }
-
-  //Serial.println("Connected to the network");
-  IPAddress ip = WiFi.localIP();
-  //Serial.print("IP Address: ");
-  //Serial.println(ip);
-  //Serial.println();
-
-  //PingServer();
-  //connectMySQL();
 }
-
-void PingServer() {
-
-  Serial.print("Pinging to host server: ");
-  Serial.println(remote_ip);
-  pingResult = WiFi.ping(remote_ip);
-  if (pingResult >= 0) {
-    Serial.print("Connected! RTT = ");
-    Serial.print(pingResult);
-    Serial.println(" ms");
-    Serial.println();
-  }
-  else {
-    Serial.print("Connection failed! Error code: ");
-    Serial.println(pingResult);
-    Serial.println();
-  }
-}
-
-/**
-void connectMySQL() {
-  Serial.println("Connecting to MySQL Server...");
-  if(conn.connect(remote_ip,3306,user,password)) 
-  {
-    delay(1000);
-    Serial1.println("Connected to MySQL.");
-  }
-  else
-    Serial.println("Connection to MySQL has failed.");
-
-  // conn.close(); 
-}
-**/
 
 void EntranceGate() {
   digitalWrite(US_Entrance_Trig, LOW);
@@ -273,23 +216,12 @@ void IR2_ScanVehicle() {
   }
 }
 
-/**
-void execQuery() {
-  char SQL_QUERY[] = "UPDATE iot_parking.parking SET isAvailable = '0' WHERE (level = '1' AND lot = '1')";
-  
-  Serial.println("Updating column data...");
-  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-  cur_mem->execute(SQL_QUERY);
-  delete cur_mem;
-}
-**/
-
-
 void GetData1() {
   WiFiEspClient client;
   
   Serial.println("Starting connection to server...");
-  // if you get a connection, report back via serial
+  
+  // Report back via serial if you get a connection
   if (client.connect(urlPayload, 80)) {
     Serial.println("Connected to server");
     // Make a HTTP request
@@ -302,61 +234,27 @@ void GetData1() {
     client.println();
   }
 
-  // if there are incoming bytes available
-  // from the server, read them and print them
-  Serial.println("Printing incoming bytes on serial monitor");
+  // Read & print if there are incoming bytes available from the server
+  Serial.println("Printing incoming bytes on serial monitor...");
   while (client.available()) {
     char c = client.read();
     Serial.write(c);
   }
 
-  // if the server's disconnected, stop the client
+  // Stop the client if the server is disconnected
   if (!client.connected()) {
     Serial.println();
     Serial.println();
     Serial.println("Disconnecting from server...");
     client.stop();
   }
-  
-  /**
-  WiFiEspClient wifi;
-  //MySQL_Connection conn((Client *)&wifi);
-  HttpClient client = HttpClient(wifi, serverAddress, port);
-  
-  Serial.println("POST Request Init");
-  // http://192.168.1.17/post_sensor_data.php?api_key=tPmAT5Ab3j7F9&lot=1&level=1&isavailable=0
-  //String postData = "api_key=" + apiKeyValue + "&lot=" + int(1) + "&level=" + int(1) + "&isavailable=" + ScanLot1;
-  String postData = "api_key=tPmAT5Ab3j7F9&lot=1&level=1&isavailable=0";
-  Serial.print("POST Query: ");
-  Serial.println(postData);
-
-  client.beginRequest();
-  client.post("/");
-  client.sendHeader("Content-Type", "application/x-www-form-urlencoded");
-  client.sendHeader("Content-Length", postData.length());
-  client.sendHeader("X-Custom-Header", "Data from Arduino");
-  client.beginBody();
-  client.print(postData);
-  client.endRequest();
-
-  // read the status code and body of the response
-  int statusCode = client.responseStatusCode();
-  String response = client.responseBody();
-
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.print("Response: ");
-  Serial.println(response);
-  Serial.println();
-  delay(1000); // Send an HTTP POST request every 1 second.
-**/
 }
 
 void GetData2() {
   WiFiEspClient client;
   
   Serial.println("Starting connection to server...");
-  // if you get a connection, report back via serial
+  // Report back via serial if you get a connection
   if (client.connect(urlPayload, 80)) {
     Serial.println("Connected to server");
     // Make a HTTP request
@@ -369,15 +267,14 @@ void GetData2() {
     client.println();
   }
 
-  // if there are incoming bytes available
-  // from the server, read them and print them
+  // Read & print if there are incoming bytes available
   Serial.println("Printing incoming bytes on serial monitor");
   while (client.available()) {
     char c = client.read();
     Serial.write(c);
   }
 
-  // if the server's disconnected, stop the client
+  // Stop the client if the server is disconnected
   if (!client.connected()) {
     Serial.println();
     Serial.println();
@@ -386,26 +283,11 @@ void GetData2() {
   }
 }
 
-/**
-  void TestUltrasonicSensor() {
-  digitalWrite(US_Exit_Trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(US_Exit_Trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(US_Exit_Trig, LOW);
-  duration_2 = pulseIn(US_Exit_Echo, HIGH);
-  distance_2 = duration_2 * 0.034 / 2;
-  Serial.print("Distance: ");
-  Serial.print(distance_2);
-  Serial.println(" cm");
-  }
-**/
-
 void loop() {
-  //EntranceGate();
-  //delay(100);
-  //ExitGate();
-  //delay(100);
+  EntranceGate();
+  delay(100);
+  ExitGate();
+  delay(100);
   IR1_ScanVehicle();
   delay(1000);
   IR2_ScanVehicle();
@@ -414,6 +296,4 @@ void loop() {
   delay(1000);
   GetData2();
   delay(1000);
-  //TestUltrasonicSensor();
-  //delay(1000);
 }
